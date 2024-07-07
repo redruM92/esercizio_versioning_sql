58013f687d2d4fd9f566b6b5b0f0067ff4a00f3d
Samuel <bibo@geemail.xyz>
2024-07-07 16:34:06 +0200

    revisione struttura database, aggiunta tabella lazio e rinominato tabella toscana

--- creazione_DB_tab.sql
+++ creazione_DB_tab.sql
@@ -1,6 +1,13 @@
 CREATE DATABASE testDB;
 
-CREATE TABLE table_testDB (
+CREATE TABLE toscana (
+    columnID int NOT NULL,
+    columnA varchar(255) NOT NULL,
+    columnB varchar(255),
+    PRIMARY KEY (columnID)
+);
+
+CREATE TABLE lazio (
     columnID int NOT NULL,
     columnA varchar(255) NOT NULL,
     columnB varchar(255),
---
+++ lazio.sql
@@ -0,0 +1,7 @@
+INSERT TO lazio
+VALUES 
+(1, "Roma", "RM")
+(2, "Latina", "LT")
+(3, "Frosinone", "FR")
+(4, "Viterbo", "VT")
+(5, "Rieti", "RI")
